c451379d12453df8f0cdf2fad3fdb0c3eef3ab6a
yuichi sugiura <sugiura@winas.jp>
2020-12-09 08:27:51 +0900

    brands追加

--- Day3Homework/Day3Homework/Components/DetailView.swift
+++ Day3Homework/Day3Homework/Components/DetailView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import BonMot
 
 protocol DetailViewDelegate: class {
     func buttonTouchUpInside(_ view: DetailView)
@@ -15,7 +16,7 @@ class DetailView: UIView {
     
     // TODO : implement this
     weak var delegate: DetailViewDelegate?
-    
+        
     var content: Content? {
         didSet{
             update()
@@ -24,7 +25,86 @@ class DetailView: UIView {
     
     func update(){
         guard let content = self.content else {return}
-        nameLabel.text  = content.name
+        
+        // name
+        let text = "<bold>\(content.name)</bold>"
+        var attributedString: NSAttributedString = text.styled(with: StringStyle(
+                .font(UIFont.default(20)),
+                .color(.black),
+                .lineSpacing(6),
+                .xmlRules([
+                    .style("bold", StringStyle(
+                        .font(UIFont.defaultBold(20)),
+                        .color(.black)
+                    )),
+                ])
+            ))
+        nameLabel.attributedText = attributedString
+        
+        //TODO Loop
+        //twitter
+        var favoriteApp = BrandIcon.twitter
+        var textbrands = "<brand>\(favoriteApp.text)</brand>"
+        var iconString : NSAttributedString = textbrands.styled(with: StringStyle(
+                .font(UIFont.default(20)),
+                .color(.black),
+                .lineSpacing(6),
+                .xmlRules([
+                    .style("brand", StringStyle(
+                        .font(UIFont.faBrand(20)),
+                        .color(favoriteApp.color)
+                    ))
+                ])
+            ))
+        
+        var textString = "@Sugiura1".styled(with: StringStyle(
+            .font(UIFont.default(15)),
+            .color(.gray)
+        ))
+        twitterLabel.attributedText = NSAttributedString.composed(of: [iconString, textString], baseStyle: StringStyle(), separator: "   ")
+       
+        //insta
+        favoriteApp = BrandIcon.instagram
+        textbrands = "<brand>\(favoriteApp.text)</brand>"
+        iconString = textbrands.styled(with: StringStyle(
+                .font(UIFont.default(20)),
+                .color(.black),
+                .lineSpacing(6),
+                .xmlRules([
+                    .style("brand", StringStyle(
+                        .font(UIFont.faBrand(20)),
+                        .color(favoriteApp.color)
+                    ))
+                ])
+            ))
+        
+        textString = "@Sugiura1".styled(with: StringStyle(
+            .font(UIFont.default(15)),
+            .color(.gray)
+        ))
+        instaLabel.attributedText = NSAttributedString.composed(of: [iconString, textString], baseStyle: StringStyle(), separator: "   ")
+        
+        //reddit
+        favoriteApp = BrandIcon.reddit
+                textbrands = "<brand>\(favoriteApp.text)</brand>"
+                iconString = textbrands.styled(with: StringStyle(
+                        .font(UIFont.default(20)),
+                        .color(.black),
+                        .lineSpacing(6),
+                        .xmlRules([
+                            .style("brand", StringStyle(
+                                .font(UIFont.faBrand(20)),
+                                .color(favoriteApp.color)
+                            ))
+                        ])
+                    ))
+                
+                textString = "@Sugiura1".styled(with: StringStyle(
+                    .font(UIFont.default(15)),
+                    .color(.gray)
+                ))
+        redditLabel.attributedText = NSAttributedString.composed(of: [iconString, textString], baseStyle: StringStyle(), separator: "   ")
+        // nameLabel.text  = content.name
         introLabel.text = content.introduction
         mainImage.image = UIImage(named: content.imageName)
         
@@ -44,6 +124,11 @@ class DetailView: UIView {
     @IBOutlet private dynamic weak var nameLabel: UILabel!
     @IBOutlet private dynamic weak var introLabel: UILabel!
     @IBOutlet private dynamic weak var mainImage: UIImageView!
+    @IBOutlet private dynamic weak var brandView: UIView!
+    @IBOutlet private dynamic weak var twitterLabel: UILabel!
+    @IBOutlet private dynamic weak var instaLabel: UILabel!
+    @IBOutlet private dynamic weak var redditLabel: UILabel!
+    
     @IBAction private func buttonTouchUpInside(_ sender: UIButton){
         delegate?.buttonTouchUpInside(self)
     }
