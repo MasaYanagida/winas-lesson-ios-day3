49ea5f1598725890dd1d7f9fde4c5ca14f50b147
Mahbubur Rahman Mishal <mishal.rahman996@gmail.com>
2020-12-09 23:44:24 +0900

    Completed HomeWork

--- Day3Homework/Day3Homework/Components/DetailView.swift
+++ Day3Homework/Day3Homework/Components/DetailView.swift
@@ -6,10 +6,33 @@
 //
 
 import UIKit
+import BonMot
+
+protocol DetailViewDelegate: class {
+    func buttonTouchUpInside(_ view: DetailView)
+}
 
 class DetailView: UIView {
     
-    // TODO : implement this
+    weak var detailViewDelegate: DetailViewDelegate?
+    @IBOutlet private var NameLabel: UILabel!
+    @IBOutlet private var MessengerLabel: UILabel!
+    @IBOutlet private var InstagramLabel: UILabel!
+    @IBOutlet private var SnapChatLabel: UILabel!
+    @IBOutlet private var LineLabel: UILabel!
+    @IBOutlet private var RedditLabel: UILabel!
+    @IBOutlet private var WhatsAppLabel: UILabel!
+    @IBOutlet private var TwitterLabel: UILabel!
+    @IBOutlet private var IntroductionLabel: UILabel!
+    @IBAction func buttonTouchUp(_ sender: Any) {
+        detailViewDelegate?.buttonTouchUpInside(self)
+    }
+    
+    var content: Content? {
+        didSet {
+            getContent()
+        }
+    }
     
     class func createFromNib() -> DetailView {
         let xib = UINib(nibName: "DetailView", bundle: nil)
@@ -19,4 +42,61 @@ class DetailView: UIView {
         }
         return view!
     }
+    
+    func getContent() {
+        guard let content = self.content else { return }
+        NameLabel.text = content.name
+        IntroductionLabel.text = content.introduction
+        setSnsList(content)
+    }
+    
+    private func setSnsList(_ content: Content) {
+        for media in content.media {
+            switch media {
+            case .twitter:
+                TwitterLabel.isHidden = false
+                setupMediaContent(media, TwitterLabel)
+            case .instagram:
+                InstagramLabel.isHidden = false
+                setupMediaContent(media, InstagramLabel)
+            case .snapchat:
+                SnapChatLabel.isHidden = false
+                setupMediaContent(media, SnapChatLabel)
+            case .reddit:
+                RedditLabel.isHidden = false
+                setupMediaContent(media, RedditLabel)
+            case .line:
+                LineLabel.isHidden = false
+                setupMediaContent(media, LineLabel)
+            case .whatsapp:
+                WhatsAppLabel.isHidden = false
+                setupMediaContent(media, WhatsAppLabel)
+            case .fbmessenger:
+                MessengerLabel.isHidden = false
+                setupMediaContent(media, MessengerLabel)
+            }
+        }
+    }
+    
+    private func setupMediaContent(_ brand: BrandIcon, _ label: UILabel) {
+        let titleText = "<brand>\(brand.text)</brand> <bold>@\(brand.name)</bold>"
+        let attributedString: NSAttributedString = titleText.styled(with: StringStyle(
+            .font(UIFont.default(20)),
+            .color(.black),
+            .lineSpacing(6),
+            .xmlRules([
+                .style("bold", StringStyle(
+                    .font(UIFont.defaultBold(20)),
+                    .color(.lightGray)
+                )),
+                .style("brand", StringStyle(
+                    .font(UIFont.faBrand(20)),
+                    .color(brand.color)
+                ))
+            ])
+        ))
+        
+        label.attributedText = attributedString
+    }
+
 }
--- Day3Homework/Day3Homework/Components/TitleView.swift
+++ Day3Homework/Day3Homework/Components/TitleView.swift
@@ -7,7 +7,79 @@
 
 import UIKit
 import SnapKit
+import BonMot
 
 class TitleView: UIView {
-    // TODO : implement this
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        label.numberOfLines   = 0
+        label.textColor       = .black
+        addSubview(label)
+        
+        return label
+    }()
+    
+    private lazy var borderView: UIView = {
+        let borderView = UIView()
+        borderView.backgroundColor = .gray
+        addSubview(borderView)
+        
+        return borderView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.placeUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.placeUI()
+    }
+    
+    private func placeUI() {
+
+        label.snp.makeConstraints { make in
+            make.top.equalTo(25)
+            make.left.equalTo(15)
+            make.right.equalTo(-15)
+            make.bottom.equalTo(-15)
+            make.height.lessThanOrEqualTo(100)
+        }
+
+        borderView.snp.makeConstraints { make in
+            make.bottom.equalTo(0)
+            make.right.equalTo(-15)
+            make.left.equalTo(15)
+            make.height.equalTo(0.5)
+        }
+    }
+    
+    func setTitle() {
+        let brandIcon: BrandIcon? = BrandIcon.twitter
+        let titleText = "\u{1F4A1}<brand>\(brandIcon!.text)</brand> <bold>\(brandIcon!.name)</bold>"
+        let attributedString: NSAttributedString = titleText.styled(with: StringStyle(
+            .font(UIFont.default(20)),
+            .color(.black),
+            .lineSpacing(6),
+            .xmlRules([
+                .style("bold", StringStyle(
+                    .font(UIFont.defaultBold(20)),
+                    .color(brandIcon!.color)
+                )),
+                .style("brand", StringStyle(
+                    .font(UIFont.faBrand(20)),
+                    .color(brandIcon!.color)
+                ))
+            ])
+        ))
+        let textString = "からのコンテンツです".styled(with: StringStyle(
+            .font(UIFont.default(20)),
+            .color(.black)
+        ))
+        label.attributedText = NSAttributedString.composed(of: [attributedString, textString], baseStyle: StringStyle(), separator: "")
+    }
+
 }
